8e3a16abc5e922b41503ccb9823205e91b053873
Luckylau <laujunbupt0203@163.com>
2017-03-02 14:05:44 +0800

    add   log   service

--- webdemo/api/config.py
+++ webdemo/api/config.py
@@ -1,5 +1,24 @@
-app={
-  'root':'webdemo.api.controllers.root.RootController',
-  'modules':['webdemo.api'],
-  'debug':True,
+app = {
+    'root': 'webdemo.api.controllers.root.RootController',
+    'modules': ['webdemo.api'],
+    'debug': True,
+}
+logging = {
+    'root': {'level': 'INFO', 'handlers': ['console']},
+    'loggers': {
+        'webdemo': {'level': 'INFO', 'handlers': ['console']}
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        }
+    },
+    'formatters': {
+        'simple': {
+            'format': ('%(asctime)s %(levelname)-5.5s [%(name)s]'
+                       '[%(threadName)s] %(message)s')
+        }
+    }
 }
--- webdemo/api/controllers/root.py
+++ webdemo/api/controllers/root.py
@@ -1,9 +1,13 @@
 from pecan import rest
 from wsme import types as wtypes
 from webdemo.api import expose
+import logging
+logger = logging.getLogger(__name__)
+
 
 class RootController(rest.RestController):
 
     @expose.expose(wtypes.text)
     def get(self):
+        logger.info("Method is called ...")
         return "python-web-frame: pecan & wsme "
